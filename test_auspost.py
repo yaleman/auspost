3e9c36d325babc901583196caa2f5b44aae3d76d
James Hodgkinson <james@terminaloutcomes.com>
2022-03-29 22:15:37 +1000

    this is getting silly

--- test_auspost.py
+++ test_auspost.py
@@ -1,13 +1,14 @@
-#!/usr/bin/env python3
+"""test the auspost module"""
 
 from auspost import search_postcode
 
 def test_search_sydney_suburb():
-    """ tests a basic sydney search 
+    """ tests a basic sydney search
         for data in search_postcode("Sydney"):
             print(data.get('state'), data.get('postcode'), data.get('suburb'))
     """
     assert "SYDNEY" in [data.get('suburb') for data in search_postcode("Sydney")]
 
 def test_search_sydney_postcode():
+    """ searching for sydney postcodes """
     assert 2000 in [data.get('postcode') for data in search_postcode("Sydney")]
